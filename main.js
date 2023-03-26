20178d45d3b4d456d2ae9374ea20b636a94b5107
Rico21x <105686982+Rico21x@users.noreply.github.com>
2023-03-26 15:35:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Ricardo Alvarez");
+console.log("2003");
+console.log(2003);
+console.log("Ricardo Alvarez (2003)");
+console.log("Ricardo");
+console.log("Alvarez");
+console.log("(2003)");
+console.log("Ricardo" + " Alvarez" + "(2003)");
+console.log("Ricardo " + " Alvarez " + "(2003)");
+
